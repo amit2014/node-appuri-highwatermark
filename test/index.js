1477f9c46e10876a9d18dd9a2970b4ba21b71572
Tom Macie <tom@appuri.com>
2016-08-04 17:45:37 +0000

    Common highwatermark functions for loaders

---
+++ test/index.js
@@ -0,0 +1,96 @@
+'use strict'
+
+const proxyquire = require('proxyquire'),
+      appuriHighwatermark = require('../index')
+
+require('chai').should()
+
+describe('getHighwatermark', () => {
+  it('should return options.override if it is set', function*() {
+    const highwatermark = yield appuriHighwatermark.getHighwatermark('', '', { override: 'override', default: 'default' })
+    highwatermark.should.equal('override')
+  })
+
+  it('should return the options.default if there is no high watermark in s3', function*() {
+    let called = 0
+    const mockedAppuriHighwatermark = proxyquire('../index', { 'aws-sdk': { S3: class {
+      getObject(params) {
+
+        called++
+        params.should.deep.equal({ Bucket: 'bucket', Key: 'key' })
+        return { promise: () => Promise.reject({ statusCode: 404 }) }
+      }}}})
+
+    const highwatermark = yield mockedAppuriHighwatermark.getHighwatermark('bucket', 'key', { default: 'default' })
+    highwatermark.should.equal('default')
+    called.should.equal(1)
+  })
+
+  it('should return the value from s3', function*() {
+    let called = 0
+    const mockedAppuriHighwatermark = proxyquire('../index', { 'aws-sdk': { S3: class {
+      getObject(params) {
+
+        called++
+        params.should.deep.equal({ Bucket: 'bucket', Key: 'key' })
+        return { promise: () => Promise.resolve({ Body: new Buffer('s3_highwatermark') }) }
+      }}}})
+
+    const highwatermark = yield mockedAppuriHighwatermark.getHighwatermark('bucket', 'key')
+    highwatermark.should.equal('s3_highwatermark')
+    called.should.equal(1)
+  })
+
+  it('should throw if options.isValid is set and the highwatermark is invalid', function*() {
+    try {
+      yield appuriHighwatermark.getHighwatermark('bucket', 'key', { override: 'foo', isValid: () => false })
+      false.should.equal(true, 'Should have thrown an exception')
+    } catch(e) {
+      e.message.should.equal('Invalid high water mark')
+    }
+  })
+
+  it('should map the highwatermark if options.map is set', function*() {
+
+    const highwatermark = yield appuriHighwatermark.getHighwatermark('bucket', 'key', {
+      override: '123',
+      isValid: (string) => !Number.isNaN(string),
+      map: Number
+    })
+    highwatermark.should.equal(123)
+  })
+})
+
+describe('putHighwatermark', () => {
+
+  it('should send the highwatermark back to s3', function*() {
+
+    let called = 0
+    const mockedAppuriHighwatermark = proxyquire('../index', { 'aws-sdk': { S3: class {
+      putObject(params) {
+
+        called++
+        params.should.deep.equal({ Bucket: 'bucket', Key: 'key', Body: 'new_highwatermark' })
+        return { promise: () => Promise.resolve() }
+      }}}})
+
+    yield mockedAppuriHighwatermark.putHighwatermark('bucket', 'key', 'new_highwatermark')
+    called.should.equal(1)
+  })
+
+  it('should not throw if there is an s3 error', function*() {
+
+    let called = 0
+    const expectedDate = 4
+    const mockedAppuriHighwatermark = proxyquire('../index', { 'aws-sdk': { S3: class {
+      putObject(params) {
+
+        called++
+        params.should.deep.equal({ Bucket: 'bucket', Key: 'key', Body: 'new_highwatermark' })
+        return { promise: () => Promise.reject({ statusCode: 400 }) }
+      }}}})
+
+    yield mockedAppuriHighwatermark.putHighwatermark('bucket', 'key', 'new_highwatermark')
+    called.should.equal(1)
+  })
+})
